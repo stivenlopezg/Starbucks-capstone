f0db0f3eb02624d0c3085aec7010a8251f26133f
Stiven López Giraldo <stiven.lopez@datalytics.com>
2020-04-11 18:50:09 -0500

    archivo de configuracion con las variables usadas en el proyecto

---
+++ classification_model/config.py
@@ -0,0 +1,10 @@
+values_reward = [0, 2, 3, 5, 10]
+
+portfolio_filepath = 'data/portfolio.json'
+profile_filepath = 'data/profile.json'
+transcript_filepath = 'data/transcript.json'
+
+portfolio_outputpath = 'data/portfolio.csv'
+profile_outputpath = 'data/profile.csv'
+transcript_outputpath = 'data/transcript.csv'
+final_filepath = 'data/starsbucks_data.csv'
