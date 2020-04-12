5a1e2b4438a49262ab853cddf4fc077f203c57e8
Stiven López Giraldo <stiven.lopez@datalytics.com>
2020-04-11 23:53:17 -0500

    se crea una funcion para hacer un grafico de barras

---
+++ classification_model/plotting.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import plotly.offline as py
+import plotly.graph_objs as go
+
+py.init_notebook_mode()
+
+
+def barplot(df: pd.DataFrame, col: str, title: str, xlabel: str, ylabel: str):
+    bar_values = df[col].value_counts().sort_values(ascending=True)
+    x = bar_values.values.tolist()
+    y = [i.capitalize() for i in bar_values.index.tolist()]
+    colors = ['lightblue'] * len(x)
+    colors[-1] = 'mediumblue'
+    trace = go.Bar(x=x,
+                   y=y,
+                   marker_color=colors,
+                   orientation='h')
+    data = go.Data([trace])
+    layout = go.Layout(title=title,
+                       xaxis={'title': xlabel},
+                       yaxis={'title': ylabel},
+                       width=700,
+                       height=500)
+    figure = go.Figure(data=data, layout=layout)
+    return py.iplot(figure)
